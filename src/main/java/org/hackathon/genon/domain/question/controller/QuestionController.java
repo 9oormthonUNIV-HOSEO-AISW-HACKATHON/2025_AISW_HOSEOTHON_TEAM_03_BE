b1f84a7202ba74f938fdaaa01a194c285743165c
kwakseobang <iii6602@naver.com>
2025-11-23 08:38:25 +0900

    Chore/#53 : 안쓰는 메소드 삭제

--- src/main/java/org/hackathon/genon/domain/question/controller/QuestionController.java
+++ src/main/java/org/hackathon/genon/domain/question/controller/QuestionController.java
@@ -2,29 +2,18 @@ package org.hackathon.genon.domain.question.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.hackathon.genon.domain.question.dto.QuestionResponseDto;
-import org.hackathon.genon.domain.question.service.QuestionAiService;
 import org.hackathon.genon.domain.question.service.QuestionQueryService;
 import org.hackathon.genon.domain.question.service.QuizQuestionAnswer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class QuestionController extends QuestionDocsController {
 
-    private final QuestionAiService questionAiService;
     private final QuestionQueryService questionQueryService;
-
-    // NOTE: 임시 API, 추후 삭제 예정
-//    @PostMapping("/generate")
-//    public ResponseEntity<List<QuestionResponseDto>> generateQuestionsByAi() {
-//        List<QuestionResponseDto> result = questionAiService.generateAndReturnQuestionsWithOptions();
-//        return ResponseEntity.ok(result);
-//    }
 
     @Override
     @GetMapping("/v1/quiz/{quizId}/questions/results")
