dccf51888b932603881f906605dbf299536d40d2
kwakseobang <iii6602@naver.com>
2025-11-22 19:00:36 +0900

    git initialize

---
+++ src/main/java/org/hackathon/genon/GenonApplication.java
@@ -0,0 +1,13 @@
+package org.hackathon.genon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hackathon/genon/GenonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackathon.genon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
