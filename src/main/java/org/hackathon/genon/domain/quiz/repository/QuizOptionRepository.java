800f7a5fd72e979bdf08406950a231c6ad4f91fa
kwakseobang <iii6602@naver.com>
2025-11-22 22:18:21 +0900

    Feat/#9: 회원 가입 기능 추가

--- src/main/java/org/hackathon/genon/domain/quiz/repository/QuizOptionRepository.java
+++
@@ -1,8 +0,0 @@
-package org.hackathon.genon.domain.quiz.repository;
-
-import org.hackathon.genon.domain.quiz.entity.QuizOption;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizOptionRepository extends JpaRepository<QuizOption, Long> {
-
-}
